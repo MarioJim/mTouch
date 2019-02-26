5cfdce77c22c72a3ff937bf2a60666d77741d9c9
MarioJim <mario.emilio.j@gmail.com>
2019-02-26 08:17:08 -0600

    Created a node.js server

---
+++ server.js
@@ -0,0 +1,9 @@
+const express = require('express');
+
+const app = express();
+
+app.use(express.static(`${__dirname}/public`));
+
+app.listen(3000, () => {
+  console.log('Server is up on port 3000');
+});
