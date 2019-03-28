c1e02593be581fcf5b5a438f58b7bd7a9d688b64
MarioJim <mario.emilio.j@gmail.com>
2019-03-27 20:44:40 -0600

    Spread functions out in app.js

--- public/js/app.js
+++ public/js/app.js
@@ -3,37 +3,56 @@ const toggleNav = () => {
   $('nav').toggleClass('active');
 };
 
-$(window).load(function () {
-  $("body").removeClass("preload");
-});
+const checkUser = (user) => {
+  const regexUser = /^[a-zA-Z_\d]{6,}$/;
+  if (!regexUser.test(user)) {
+    alert('Enter a username consisting of at least 6 letters and underscores');
+    return false;
+  } else return true;
+}
+
+const checkMail = (mail) => {
+  const regexMail = /\w+@[a-zA-Z_]+?\.[a-zA-Z]{2,3}$/;
+  if (!regexMail.test(mail)) {
+    alert('Enter a valid email address');
+    return false;
+  } else return true;
+};
+
+const checkPass = (pass) => {
+  const regexPass = /.{4,}/;
+  if (!regexPass.test(pass)) {
+    alert('Password is too short');
+    return false;
+  } else return true;
+};
 
 const validateLogin = () => {
   const mail = $('#logform .mail').val();
   const pass = $('#logform .password').val();
-  if (mail.length === 0 && pass.length === 0) return { state: 'empty' };
-  const regexMail = /\w+@[a-zA-Z_]+?\.[a-zA-Z]{2,3}$/;
-  if (!regexMail.test(mail)) alert('Enter a valid email address');
-  else if (pass.length < 4) alert('Password is too short');
-  else return { state: 'valid', mail, pass };
-  return { state: '' };
+  if (mail.length === 0 && pass.length === 0)
+    return { state: 'empty' };
+  else if (checkMail(mail) && checkPass(pass))
+    return { state: 'valid', mail, pass };
+  else
+    return { state: '' };
 };
 
 const validateRegister = () => {
   const user = $('#regform .user').val();
   const mail = $('#regform .mail').val();
   const pass = $('#regform .password').val();
-  if (mail.length === 0 && pass.length === 0) return { state: 'empty' };
-  const regexUser = /^[a-zA-Z_\d]{6,}$/;
-  const regexMail = /^\w+@[a-zA-Z_]+?\.[a-zA-Z]{2,3}$/;
-  if (!regexUser.test(user)) alert('Enter a username consisting of at least 6 letters and underscores');
-  else if (!regexMail.test(mail)) alert('Enter a valid email address');
-  else if (pass.length < 4) alert('Password is too short');
-  else return { state: 'valid', user, mail, pass };
-  return { state: '' };
+  if (mail.length === 0 && pass.length === 0)
+    return { state: 'empty' };
+  else if (checkUser(user) && checkMail(mail) && checkPass(pass))
+    return { state: 'valid', user, mail, pass };
+  else
+    return { state: '' };
 };
 
 $(document).ready(() => {
-  $('#logbtn').on('click', () => {
+  $("body").removeClass("preload");
+  $('#logbtn').on('click touch', () => {
     const result = validateLogin();
     switch (result.state) {
       case 'valid':
@@ -48,7 +67,7 @@ $(document).ready(() => {
       default:
     }
   });
-  $('#regbtn').on('click', () => {
+  $('#regbtn').on('click touch', () => {
     const result = validateRegister();
     switch (result.state) {
       case 'valid':
